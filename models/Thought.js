1dc8d33dc9f694181d2b90c70362166ff8bcc6cf
blessing-o <oghaleokeme@gmail.com>
2024-01-26 15:27:27 -0500

    created the model for users, thoughts and reactions

---
+++ models/Thought.js
@@ -0,0 +1,44 @@
+const { Schema, model } = require("mongoose");
+const reactionSchema = require('./Reaction')
+
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      minLength: 1,
+      maxLength: 280,
+
+    },
+
+    createdAt: {
+      type: Date,
+      //Do the other ones
+      default: Date.now,
+    },
+
+    username:
+      {
+        type: String,
+        required: true,
+      },
+
+    reactions: [reactionSchema],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+
+    id: false,
+  }
+);
+
+userSchema.virtual("reactionCount").get(function () {
+  return this.reaction.length;
+});
+
+const thought = model('thought',thoughtSchema);
+
+module.exports = thought;
